28ac4fe30cc2808e250f8132a76dbd8790a441d0
Eugene Krashtan <eugene.krashtan@auto-talks.com>
2025-11-22 15:34:35 +0200

    Compiled base

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # foc
 FOC testing
+
+## Init
+
+west init -m ssh://git@github.com/Delsian/foc.git --manifest-rev=main foc
+
+## Build
+
+west build -b foc_431 -d build_foc .
--- fw/src/main.c
+++ fw/src/main.c
@@ -1,19 +1,10 @@
-#include <dk_buttons_and_leds.h>
 #include <zephyr/logging/log.h>
 
 LOG_MODULE_REGISTER(main, LOG_LEVEL_DBG);
 
 int main(void)
 {
-    int err;
-
     LOG_INF("Starting");
-
-    err = dk_leds_init();
-    if (err) {
-        LOG_ERR("LEDs init failed (err %d)", err);
-        return err;
-    }
 
     while (1) {
         k_sleep(K_FOREVER);
